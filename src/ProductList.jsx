68c48610fe86888ad25d9dc0a666e906bbb1ff1c
duonggiang00 <duonggiang00@gmail.com>
2025-05-19 09:34:39 +0700

    hehe

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -7,21 +7,29 @@ const url = "https://dummyjson.com/products";
 const ProductList = () => {
     const [products, setProducts] = useState([]);
     const [page, setPage] = useState(1);
-    const [limit, setLimit]= useState(6)
+    const [limit, setLimit]= useState(6);
+    const [search,setSearch] = useState('');
+    const [sort,setSort] = useState('')
     const handlePage = (data)=>{
         return data==="next" ? page <=Math.ceil(194/limit) && setPage(page+1) : page > 1 && setPage(page-1);
     }
     const handleLimit = (e)=>{
         setLimit(+e.target.value)
     }
+    const handleSort = (e)=>{
+        setSort(e.target.value)
+    }
+    const handleSearch = (e)=>{
+        setSort(e.target.value)
+    }
     const fetchAPI = async ()=>{
-        const res = await fetch(`${url}?limit=${limit}&skip=${(page-1)*limit}`)
+        const res = !sort ? await fetch(`${url}?limit=${limit}&skip=${(page-1)*limit}`) : await fetch(`${url}?limit=${limit}&skip=${(page-1)*limit}&sortBy=price&order=${sort}`)
         const data = await res.json();
         setProducts(data.products);
     };
     useEffect(()=>{
         fetchAPI();
-    },[page,limit])
+    },[page,limit,sort])
     
 
 
@@ -31,7 +39,8 @@ const ProductList = () => {
     <div>
         <h1>Danh sach san pham</h1>
             <div className="filter">
-                <select name="sort" id="sort">
+                <input type="input" id='search' name='search' onInput={handleSearch}/>
+                <select name="sort" id="sort" onChange={handleSort}>
                     <option value="">default</option>
                     <option value="asc">asc</option>
                     <option value="desc">desc</option>
@@ -44,7 +53,7 @@ const ProductList = () => {
             </div>
 			<div>
 				{products.map((item) => (
-					<div key={item.id}>{item.title}</div>
+					<div key={item.id}>${item.id} {item.title} {item.price}</div>
 				))}
 			</div>
 			<button onClick={() => handlePage("preview")}>preview</button>
